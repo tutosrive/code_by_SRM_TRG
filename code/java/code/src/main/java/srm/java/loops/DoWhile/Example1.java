8048ba3cb85052eed36706e05e99b438b2d46d8b
Tutos Rive Gamer <108991712+tutosrivegamerLQ@users.noreply.github.com>
2024-11-11 21:07:59 -0500

    Update Example1.java

--- code/java/code/src/main/java/srm/java/loops/DoWhile/Example1.java
+++ code/java/code/src/main/java/srm/java/loops/DoWhile/Example1.java
@@ -10,20 +10,32 @@ import srm.java.own.Printer;
 
 public class Example1 {
     public static void main(String[] args) {
+        // Variables que serán usadas
         String passOriginal = "AsñDf", passUser;
+        // Variable bandera ver (https://keepcoding.io/blog/uso-de-banderas-en-bucles-while/#%C2%BFQue_son_las_banderas_en_bucles_while)
         boolean correctPass = false;
 
+        // Ejecutar bloque al menos una vez
         do {
+            // Leer datos por teclado (contraseña introducida por el usuario)
             passUser = Keyboard.readT("Ingrese la contraseña: ");
 
+            // Verificar si la contraseña ingresada es igual a la contraseña requerida
             if (passOriginal.equals(passUser)) {
+                // Si son iguales, la bandera será true, permitiendo así salir del bucle sin "break"
                 correctPass = true;
-            } else {
+            } 
+            // Si las contraseñas no coinciden
+            else {
+                // Mostrar mensajes que orienten al usuario
                 Printer.werror("¡Contraseña incorrecta!");
                 Printer.warning("Intente nuevamente: ");
             }
-        } while (!correctPass);
+        }
+        // Finalmente se verifica si la bandera sigue siendo falsa, caso contrario sale
+        while (!correctPass);
 
+        // Información que verifica la salida correcta
         Printer.winfo("¡Bienvenido al sistema!");
         Printer.warning("Loading...");
     }
